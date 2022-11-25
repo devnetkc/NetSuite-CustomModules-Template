f3bad43cd93e4e2e3cd2af555c1af0b1bd252b3c
Ryan Valizan <ryan@vnetkc.com>
2022-11-25 16:15:30 -0600

    💡 added typedef notes

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
-/** @format */
+/**
+ * @format
+ * @name JestConfig
+ * @description Jest config file for project
+ * @exports BuildJestConfig
+ */
 
 const SuiteCloudJestConfiguration = require('@oracle/suitecloud-unit-testing/jest-configuration/SuiteCloudJestConfiguration');
 const cliConfig = require('./suitecloud.config');
@@ -10,8 +15,19 @@ const CustomJestStubs = require('@vnetwork-solutions/ns-custom-stubs-template/cu
 const SuiteCloudJestCustomStubs = SuiteCloudJestStubs.customStubs;
 // Add your custom stubs to SuiteCloudJestStubs
 const MyCustomStubs = CustomJestStubs.customStubs;
-
-// Here, you can control the paths for stubbed modules to their local file for Jest tests.
+/**
+ * @typedef ExcludeStub
+ * @type {Object}
+ * @property {String} name - "Directory/Path/ModuleName" -- CustomStub property to use
+ * @property {String} path - Path to module for project -- generally you will use `\`${__dirname}/src/FileCabinet\``
+ * @description Array of module names and paths to use for them in the stubs package
+ * @summary Here, you can control the paths for stubbed modules to their local file for Jest tests.
+ */
+/**
+ * @name ExcludeStubs
+ * @type {ExcludeStub[]}
+ * @description Array of modules from stub pkg to override with local version
+ */
 const ExcludeStubs = [
   {
     name: '/SuiteScripts/Modules/aModule',
@@ -22,16 +38,22 @@ const ExcludeStubs = [
     path: `${__dirname}/src/FileCabinet`,
   },
 ];
-const TestArray = SuiteCloudJestCustomStubs.concat(MyCustomStubs(ExcludeStubs)); //?
-//SuiteCloudJestCustomStubs.push(cfmCustomStubs); //?
 
-module.exports = SuiteCloudJestConfiguration.build({
+/**
+ * @name BuildJestConfig
+ * @function
+ * @description Builds Jest config file using stubs and SuiteCloud config
+ * @since 2022.2
+ */
+const BuildJestConfig = SuiteCloudJestConfiguration.build({
   testResultsProcessor: './node_modules/jest-junit-reporter',
   projectFolder: cliConfig.defaultProjectFolder,
   projectType: SuiteCloudJestConfiguration.ProjectType.ACP,
-  customStubs: TestArray,
+  customStubs: SuiteCloudJestCustomStubs.concat(MyCustomStubs(ExcludeStubs)),
   testPathIgnorePatterns: [
     '(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
     '**.*\\.min\\.*',
   ],
 });
+
+module.exports = BuildJestConfig;
