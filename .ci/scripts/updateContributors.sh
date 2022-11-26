502f3c3b7001cdbd81a1de878d7d9c1dddff8e85
Ryan Valizan <ryan@vnetkc.com>
2022-11-26 15:02:55 -0600

    ✏️ needed pointer to be in all caps

--- .ci/scripts/updateContributors.sh
+++ .ci/scripts/updateContributors.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo $(pwd)
-SCRIPT_DIR=$(pwd)
+
 # Assign the filename
-README_TPL="$SCRIPT_DIR/.ci/templates/README.hbs"
-README_TPL_TEMP="$SCRIPT_DIR/.ci/templates/README.temp.hbs"
+README_TPL="./.ci/templates/README.hbs"
+README_TPL_TEMP="./.ci/templates/README.temp.hbs"
+
 # Create temporary files
-CONTRIBUTORS_FILE="$SCRIPT_DIR/.ci/templates/contributors.temp.md"
+CONTRIBUTORS_FILE="./.ci/templates/contributors.temp.md"
 cp $README_TPL $README_TPL_TEMP
 touch $CONTRIBUTORS_FILE
 
@@ -18,10 +18,9 @@ sed -i "" s/"## Contributors"/"## 👥 Contributors"/ $CONTRIBUTORS_FILE
 # Store our files to merge
 README_OUTPUT=$(<$README_TPL)
 CONTRIBUTOR_OUTPUT=$(<$CONTRIBUTORS_FILE)
+
 # Merge files replacing pointer in TPL file
-echo $README_OUTPUT
-echo $CONTRIBUTOR_OUTPUT
-echo "${README_OUTPUT//"{{Contributors}}"/$CONTRIBUTOR_OUTPUT}" >$README_TPL_TEMP
+echo "${README_OUTPUT//"{{CONTRIBUTORS}}"/$CONTRIBUTOR_OUTPUT}" >$README_TPL_TEMP
 
 # Clean up contributor temp file
 rm $CONTRIBUTORS_FILE
