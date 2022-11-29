c28951ed1a85bf9a3ad5237ec778dc7c8e967804
Ryan Valizan <ryan@vnetkc.com>
2022-11-28 21:51:55 -0600

    🐞 was not pushing changes if commit reverted

--- .ci/scripts/createDeployment.sh
+++ .ci/scripts/createDeployment.sh
@@ -1,67 +1,94 @@
 #!/bin/bash
-#>testFile.txt
 ## Reset Manifest
 cp ./.ci/templates/manifest.tpl.xml src/manifest.xml
+
+## Store branch name for release
+echo "$2" >./.ci/.branch
+
+## Declare variables
+### Create xml schema variables
 endOfLine="</path>\n"
+
+### Create regex strings to test with
 srcRegex='src\/.*'
 attributesRegex='.*\.attributes.*'
 fileRegex='src\/FileCabinet\/SuiteScripts.*'
 objectRegex='src\/Objects\/.*'
 objectScriptPathRegex='.*scriptfile.*'
+
+### Output strings for File and Object lines
+#### Add more here if using other branches of deploy.xml schema
 FILELINE=
 OBJECTLINE=
+## Read git log for files impacted since origin headbranch -- reverts are also in the log
 while read line; do
-    # We only want to update src files in deployment
+    ### We only want to update src files in deployment
     if [[ "$line" =~ $srcRegex ]]; then
-        # Setup what the new line will look like
+        ### Test if file exists, if not we can skip it and import as dependency later
+        if ! test -f "$line"; then
+            continue
+        fi
 
+        ### Setup what the new line will look like
         newLine=$(echo "$line" | sed "s/src\//\t<path>~\//")"$endOfLine"
-        # Is a src file
-        # Separate Files & Objects to build deploy.xml
-        #echo "$line"
+
+        ### Skip duplicates -- lines which are already prepped to go in to deploy.xml
+        if [[ $OBJECTLINE == *"$newLine"* || $FILELINE == *"$newLine"* ]]; then
+            continue
+        fi
+
+        ### Separate Files & Objects to build deploy.xml
         if [[ "$line" =~ $fileRegex ]]; then
             if [[ "$line" =~ $attributesRegex ]]; then
                 continue
-            elif ! test -f "$line"; then
-                continue
             fi
-            #echo "file regex match"
-            # Is a file update
+            #### Is a file update
             FILELINE+="\t${newLine}"
-            # echo $FILELINE
         elif [[ "$line" =~ $objectRegex ]]; then
-            # Test line to see if the object needs a file path added
+            ### Test line to see if the object needs a file path dependency added
             while read OBJLINE; do
                 if [[ "$OBJLINE" =~ $objectScriptPathRegex ]]; then
-                    #regexStr=".*${OBJLINE}.*"
-                    #echo $OBJLINE
+                    #### Modify line to deploy.xml schema
                     SCRIPTPATH=$(echo "$OBJLINE" | sed "s/<scriptfile>\[/\/src/")
                     SCRIPTPATH=$(echo "${SCRIPTPATH}" | sed "s/\]<\/scriptfile>//")
                     depLine=$(echo "${SCRIPTPATH}" | sed "s/\/src\//\t<path>~\/FileCabinet\//")"$endOfLine"
                     CLEANPATH=$(echo "$SCRIPTPATH" | sed "s/src\///")
                     regexLineStr=".*$CLEANPATH"
+
+                    #### Check if dependency file is already in file paths list
                     if [[ "$FILELINE" =~ $regexLineStr ]]; then
                         continue
                     fi
+
+                    ##### Add to dependency to file list
                     FILELINE+="\t${depLine}"
                 fi
             done < <(cat "./${line}")
-            #echo "object regex match"
-            # Is an object file update
+
+            #### Is an object file update
             OBJECTLINE+="\t${newLine}"
-            #echo $OBJECTLINE
         fi
     fi
+    ### Clear line variable
     line=""
-done < <(git diff origin/$1 $2 --name-only)
+done < <(git log --oneline --stat origin/$1..$2 --name-only)
 
+## Generate deploy.xml file
+### Define output string variable
 OUTPUTLINES=""
+
+### If file path entries is > 0 add files to deploy.xml
 if [[ $FILELINE ]]; then
     OUTPUTLINES="<files>\n${FILELINE}\t</files>"
 fi
+
+### If object path entries is > 0 add objects to deploy.xml
 if [[ $OBJECTLINE ]]; then
     OUTPUTLINES="${OUTPUTLINES}\n\t<objects>\n${OBJECTLINE}\t</objects>"
 fi
+
+### Print output to custom updated deploy.xml
+# shellcheck disable=SC2059
 printf "<deploy>
     <configuration>
         <path>~/AccountConfiguration/*</path>
@@ -70,4 +97,7 @@ printf "<deploy>
     <translationimports>
         <path>~/Translations/*</path>
     </translationimports>
-</deploy>" >src/deploy.xml | cat src/deploy.xml
+</deploy>" >src/deploy.xml
+
+## Echo output for logging
+cat src/deploy.xml
